c0a79df479a653df5c450d62549894dd34b776da
m4rc0k15 <98577629+m4rc0k15@users.noreply.github.com>
2022-02-10 07:54:11 +0100

    add separator

--- flexport_modul.py
+++ flexport_modul.py
@@ -2,11 +2,12 @@ class flxp():
     """ Klasse für den Export von Werten in eine CSV Datei
         Class for export data into csv file
 
-    flexport requires the following parameters:
+    flexport requires the following parameters to create the object:
 
         path        --> e.g. C:\target-export-folder-name        don´t close the path with an \ at the end
         filename    --> e.g. testfilename                        don´t use a extension
         extension   --> e.g. txt or csv                          don´t use a . (dot) in the extension variable
+        separator   --> e.g. ; (semicolon)
 
     The methode .test only prints the complete path and filename in the shell and prints the content of variable data
     Variable data is to send into the .test methode and must be a list e.g. "Value","Value2","Value3"
@@ -17,35 +18,48 @@ class flxp():
 
     Variable wp ist to choose w = overwrite existing file or a = append record to existing file
 
-    The methode .export is the main methode to use
-    Modul .export expects a variable data too with a list of strings
-    flexport.export(data,wp)
+    The methode .flexport is the main methode to export data into a file
+    Modul .flexport expects a variable data which is a list with strings
+    use flexport.export(data,wp)
+
+    The modul file flexport_modul.py must be located in the project folder.
+
+    Example Use:
+
+    import flexport_modul as flp
+    pfad = r"Z:\Python Projekte Code\tests"
+    fp = flp.flxp(pfad, "testdatei", "txt",";")
+    fp.flexport(["1234","4545","4545"], "a")
+
 
     """
     
-    def __init__(self, path, filename, extension):
+    def __init__(self, path, filename, extension, separator):
         self.path = path + "\\"
         self.filename = filename
         self.extension = extension
+        self.separator = separator
 
     def test(self, data,wp):
         print("Test")
         print("Export into "+self.path + self.filename + "." + self.extension)
         print("Write modus = "+wp)
+        print("Separator: " + self.separator)
         print("")
-        print(data)
+        dataline = self.separator.join(data)
+        print(datalin)
 
     def flexport(self,data,wp):
         try:
             file = open(self.path + self.filename + "." + self.extension,"r")
             file.close()
             file = open(self.path + self.filename + "." + self.extension,wp)
-            dataline = ";".join(data)
+            dataline = self.separator.join(data)
             file.write("\n"+dataline)
             file.close()
   
         except:
             file = open(self.path + self.filename + "." + self.extension,wp)
-            dataline = ";".join(data)
+            dataline = self.separator.join(data)
             file.write(dataline)
             file.close()
